bdf011b91f72ed9411cb5d515a785d5f9d799ef3
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-17 19:42:39 -0300

    Add navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,39 @@
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Wrapper } from "@/css";
 
 export const Navbar: FunctionComponent = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <ListContainer>
+          <ListItem>
+            <Link to="/">Home</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/other">Other page</Link>
+          </ListItem>
+        </ListContainer>
+      </Wrapper>
+    </Container>
+  );
 };
+
+const Container = styled.nav`
+  height: 80px;
+  display: flex;
+  align-items: center;
+`;
+
+const ListContainer = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  a {
+    color: ${({ theme }) => theme.assets.primary};
+  }
+`;
--- src/containers/Other/Other.tsx
+++ src/containers/Other/Other.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
 
 export const Other: FunctionComponent = () => {
-  return <div>Other</div>;
+  return <div>Other page</div>;
 };
