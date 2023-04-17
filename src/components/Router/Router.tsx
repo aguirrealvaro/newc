e835e13a18a5f09094b44538373cd01e955e734d
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-04-17 19:32:26 -0300

    Add 'Other' Route

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "@/containers";
+import { Home, Other } from "@/containers";
 
 export const Router: FunctionComponent = () => {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path="other" element={<Home />} />
+      <Route path="other" element={<Other />} />
     </Routes>
   );
 };
---
+++ src/containers/Other/Other.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from "react";
+
+export const Other: FunctionComponent = () => {
+  return <div>Other</div>;
+};
